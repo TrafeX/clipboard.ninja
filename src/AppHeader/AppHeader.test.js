3f2a2d05613111fb456d1821afac91a1071b2033
Tim de Pater <git@trafex.nl>
2017-06-14 11:01:30 +0200

    Updated node modules and removed not used import.

--- src/AppHeader/AppHeader.test.js
+++ src/AppHeader/AppHeader.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppHeader from './AppHeader';
-import {MuiThemeProvider} from 'material-ui';
 
 it.skip('renders without crashing', () => {
   const div = document.createElement('div');
