ef1e157885b8863b6bc03ac3ec54ac0b0f507953
Tim de Pater <git@trafex.nl>
2017-09-16 11:29:01 +0200

    Updated node modules and NodeJS versions. Fixed 'duplicate copies of React' error

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -92,6 +92,8 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      // Avoid the 'multiple copies of React' error
+      react: path.resolve('node_modules/react'),
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
