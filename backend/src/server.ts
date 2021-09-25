7099cb1f81f770b6ad0baab6f734e458a8cf818d
Tim de Pater <git@trafex.nl>
2021-09-25 23:21:18 +0200

    Introduce new Typescript-based backend

---
+++ backend/src/server.ts
@@ -0,0 +1,92 @@
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+    cors: {
+        origin: ["http://localhost:3000", "https://clipboard.ninja"],
+        methods: ["GET", "POST"]
+    }
+});
+
+interface roomIds {
+    [index: string]: number;
+}
+interface mySocket extends Socket {
+    roomNr?: number;
+    roomId?: string;
+}
+
+const roomIds = {} as roomIds;
+let nrUsers = 0;
+
+function randomInt(low: number, high: number) {
+    const number = Math.floor(Math.random() * (high - low) + low);
+    let nrLoops = 0;
+    do {
+        if (++nrLoops > 5) {
+            return new Error('All room numbers are taken');
+        }
+    } while (typeof(roomIds['room-' + number]) !== 'undefined');
+
+    return number;
+}
+
+io.on("connection", (socket: mySocket) => {
+    const roomNr = randomInt(100000, 999999);
+    if (roomNr instanceof Error) {
+        console.log('ERROR:', roomNr);
+        return;
+    }
+    socket.roomNr = roomNr;
+    socket.roomId = 'room-' + socket.roomNr;
+    socket.join(socket.roomId);
+    socket.emit('registered', socket.roomNr);
+
+    roomIds[socket.roomId] = 1;
+    ++nrUsers;
+
+    console.log('User ' + nrUsers + ' registered in room: ' + socket.roomId);
+    socket.on('disconnect', function() {
+        if (typeof socket.roomId == "undefined") {
+            return;
+        }
+        roomIds[socket.roomId]--;
+        io.to(socket.roomId).emit('unsubscribed', roomIds[socket.roomId]);
+
+        if (roomIds[socket.roomId] <= 0) {
+            // Nobody in the room anymore, delete it
+            delete roomIds[socket.roomId];
+        }
+        --nrUsers;
+        console.log('User disconnected from ' + socket.roomId);
+    });
+    socket.on('publish', function(msg){
+        if (typeof socket.roomId == "undefined") {
+            return;
+        }
+        io.to(socket.roomId).emit('message', msg);
+    });
+    socket.on('join', function(roomNr){
+        if (typeof socket.roomId == "undefined") {
+            return;
+        }
+        if (typeof(roomIds['room-' + roomNr]) === 'undefined') {
+            socket.emit('deviceid-not-exists');
+            return;
+        }
+        // Leave current room
+        roomIds[socket.roomId]--;
+
+        // Join new room
+        socket.roomId = 'room-' + roomNr;
+        socket.join(socket.roomId);
+        roomIds[socket.roomId]++;
+
+        io.to(socket.roomId).emit('subscribed', roomNr, roomIds[socket.roomId]);
+        console.log('User in ' + socket.roomId + ' joins ' + socket.roomId);
+    });
+});
+
+httpServer.listen(3001);
+console.log('Server started');
