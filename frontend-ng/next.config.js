9c47b0e56c66d27c43f91c86f488c569459eb1a3
Tim de Pater <git@trafex.nl>
2023-04-01 10:18:40 +0200

    Add headers to improve security

--- frontend-ng/next.config.js
+++ frontend-ng/next.config.js
@@ -6,6 +6,31 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = withPWA({
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          }
+          // {
+          //   key: 'Content-Security-Policy',
+          //   value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://matomo.trafex.nl https://vitals.vercel-insights.com https://cdn.vercel-insights.com; font-src: 'self' data:; connect-src 'self' https://matomo.trafex.nl https://backend.clipboard.ninja; img-src 'self' data: blob:; style-src 'self' 'unsafe-inline'; base-uri 'self';form-action 'self'",
+          // }
+        ],
+      },
+    ]
+  }
 })
 
 module.exports = nextConfig
