d481da275b423be95a766c1a4e5ef3ec2fed3bd7
Tim de Pater <git@trafex.nl>
2025-12-14 16:52:18 +0100

    Upgrade to Next.js v16

--- frontend-ng/next.config.js
+++ frontend-ng/next.config.js
@@ -1,50 +1,51 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 
 const nextConfig = withPWA({
   reactStrictMode: true,
+  turbopack: {},
   headers: async () => {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
           },
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
+            key: "X-Content-Type-Options",
+            value: "nosniff",
           },
           {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          }
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
           // {
           //   key: 'Content-Security-Policy',
           //   value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://matomo.trafex.nl https://vitals.vercel-insights.com https://cdn.vercel-insights.com; font-src: 'self' data:; connect-src 'self' https://matomo.trafex.nl https://backend.clipboard.ninja; img-src 'self' data: blob:; style-src 'self' 'unsafe-inline'; base-uri 'self';form-action 'self'",
           // }
         ],
       },
-    ]
+    ];
   },
   redirects: async () => {
     return [
       {
-        source: '/index.html',
-        destination: '/',
+        source: "/index.html",
+        destination: "/",
         permanent: true,
       },
       {
-        source: '/about.html',
-        destination: '/about',
+        source: "/about.html",
+        destination: "/about",
         permanent: true,
       },
-    ]
-  }
-})
+    ];
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
